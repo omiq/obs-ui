f84e32048deb73d6ecc84ec20a8e9ef0ad267b69
omiq <chris@chrisg.com>
2018-09-22 13:41:26 -0600

    Graphical touch screen keyboard emulation for pi zero

--- obsui.py
+++ obsui.py
@@ -1,30 +1,80 @@
 # modules
+import time
 import tkinter as tk
 from PIL import Image, ImageTk
 
 
-def button_click():
-    """
-    respond to the button click
-    """
-    print("well it was clicked, what now?")
-
-
+# initialise tk
 root = tk.Tk()
 
-# create a frame and pack it
-frame1 = tk.Frame(root)
-frame1.pack(side=tk.TOP, fill=tk.X)
+
+# function to send the key data
+def write_report(report):
+    with open('/dev/hidg0', 'rb+') as fd:
+        fd.write(report.encode())
+
+
+# click event
+def button_click(caller):
+    print("Button: {} was clicked, sending keypress".format(caller))
+
+    if "button1" == caller:
+        write_report("\0\0\4\0\0\0\0\0")
+    elif "button2" == caller:
+        write_report("\0\0\5\0\0\0\0\0")
+    elif "button3" == caller:
+        write_report("\0\0\6\0\0\0\0\0")
+    elif "button4" == caller:
+        write_report("\0\0\7\0\0\0\0\0")
+    elif "button5" == caller:
+        write_report("\0\0\10\0\0\0\0\0")
+    else:
+        write_report("\0\0\11\0\0\0\0\0")
+
+    time.sleep(0.2)
+    write_report("\0\0\0\0\0\0\0\0")
+
 
 # load icons
-image1 = Image.open("button.png")
-photo1 = ImageTk.PhotoImage(image1)
+image = Image.open("a.png")
+photoa = ImageTk.PhotoImage(image)
 
-# create the image button, image is above (top) the optional text
-button1 = tk.Button(frame1, compound=tk.TOP, width=155, height=55, image=photo1,
-    text="optional text", bg='green', command=button_click)
-button1.pack(side=tk.LEFT, padx=2, pady=2)
-# save the button's image from garbage collection (needed?)
-button1.image = photo1
+image = Image.open("b.png")
+photob = ImageTk.PhotoImage(image)
+
+image = Image.open("c.png")
+photoc = ImageTk.PhotoImage(image)
+
+image = Image.open("d.png")
+photod = ImageTk.PhotoImage(image)
+
+image = Image.open("e.png")
+photoe = ImageTk.PhotoImage(image)
+
+image = Image.open("f.png")
+photof = ImageTk.PhotoImage(image)
+
+
+
+# add buttons
+button1 = tk.Button(root, compound=tk.CENTER, image=photoa, text="Button 1", command=lambda: button_click('button1'))
+button1.grid(row=0, column=0)
+
+button2 = tk.Button(root, compound=tk.CENTER, image=photob, text="Button 2", command=lambda: button_click('button2'))
+button2.grid(row=0, column=1)
+
+button3 = tk.Button(root, compound=tk.CENTER, image=photoc, text="Button 3", command=lambda: button_click('button3'))
+button3.grid(row=0, column=2)
+
+button4 = tk.Button(root, compound=tk.CENTER, image=photod, text="Button 4", command=lambda: button_click('button4'))
+button4.grid(row=1, column=0)
+
+button5 = tk.Button(root, compound=tk.CENTER, image=photoe, text="Button 5", command=lambda: button_click('button5'))
+button5.grid(row=1, column=1)
+
+button6 = tk.Button(root, compound=tk.CENTER, image=photof, text="Button 6", command=lambda: button_click('button6'))
+button6.grid(row=1, column=2)
+
+
 # start the event loop
 root.mainloop()
